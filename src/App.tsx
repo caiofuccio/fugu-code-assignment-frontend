e1676419f3d3e752f09d0722e3e301bae43c5521
Caio Fuccio <caio.fuccio@gmail.com>
2024-08-08 12:40:30 -0300

    feat: creates the application page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './styles.module.scss';
+import Card from './components/Card';
+import Input from './components/Input';
+import Button from './components/Button';
+import { SummaryInterface } from './interface/SummaryInterface';
+import Text from './components/Text';
+import { EmailAnalyzerService } from './services/EmailAnalyzerService';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [mailText, setMailText] = useState<string>('');
+    const [summaryData, setSummaryData] = useState<SummaryInterface>();
+    const [errorMessage, setErrorMessage] = useState<string>('');
+
+    const inputTitle = 'Message';
+    const inputPlaceholder = 'Type your message here';
+    const buttonTitle = 'Summarize';
+
+    const sendMessage = async () => {
+        try {
+            const emailAnalysis = await EmailAnalyzerService.getEmailSentiment(mailText);
+            setSummaryData(emailAnalysis);
+        } catch (error) {
+            setErrorMessage('Unable to summarize Email');
+        }
+    };
+
+    return (
+        <div className={styles['container']}>
+            <Card>
+                <h1>EMAIL SUMMARIZER</h1>
+                <Input
+                    title={inputTitle}
+                    placeholder={inputPlaceholder}
+                    text={mailText}
+                    onChange={e => setMailText(e.target.value)}
+                />
+                <Button title={buttonTitle} onClick={sendMessage} />
+                <div>
+                    {summaryData && (
+                        <div>
+                            <Text label="Summary" text={summaryData.summary} />
+                            <Text label="Sentiment" text={summaryData.sentiment} />
+                            <Text
+                                label="Awaiting Response?"
+                                text={summaryData.awaitingResponse ? 'yes' : 'no'}
+                            />
+                        </div>
+                    )}
+                </div>
+                {errorMessage && <p>{errorMessage}</p>}
+            </Card>
+        </div>
+    );
 }
 
 export default App;
