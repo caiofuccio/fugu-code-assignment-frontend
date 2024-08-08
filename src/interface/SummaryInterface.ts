71f83a7d12fa830bbb4abbeb892b4f0389696e89
Caio Fuccio <caio.fuccio@gmail.com>
2024-08-08 12:40:13 -0300

    feat: creates the email analyzer service to connect with backend

---
+++ src/interface/SummaryInterface.ts
@@ -0,0 +1,5 @@
+export interface SummaryInterface {
+    summary: string;
+    sentiment: string;
+    awaitingResponse: boolean;
+}
---
+++ src/services/EmailAnalyzerService.ts
@@ -0,0 +1,22 @@
+import { SummaryInterface } from '../interface/SummaryInterface';
+
+export class EmailAnalyzerService {
+    static async getEmailSentiment(email: string): Promise<SummaryInterface> {
+        const url = `${process.env.REACT_APP_API_URL}/analyze-email`;
+        const authToken = process.env.REACT_APP_API_AUTH_TOKEN || '';
+        const body = { emailText: email };
+
+        return await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: authToken,
+            },
+        }).then(response => {
+            if (!response.ok) throw new Error(`Unable to connect to the server`);
+
+            return response.json();
+        });
+    }
+}
